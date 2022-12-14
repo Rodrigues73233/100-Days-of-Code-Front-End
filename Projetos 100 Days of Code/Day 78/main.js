1eac10f64f9701ea39b04a2f03cfcfcd42baef44
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2022-12-14 17:35:20 -0300

    Add files via upload

---
+++ Projetos 100 Days of Code/Day 78/main.js
@@ -0,0 +1,125 @@
+// Exercicio 01
+// let resultado = document.querySelector('#resultado');
+
+// function calculaCombustivel() {
+//   let dinheiro = prompt('Quanto dinheiro você tem ?');
+//   let combustivel = dinheiro / 5;
+//   let kms = combustivel * 20
+//   resultado.innerHTML = `<h1>Você tem ${dinheiro}R$, consegue comprar ${combustivel} L de combustivel e rodar ${kms} KM com esse combustivel</h1>`
+// }
+
+// calculaCombustivel()
+
+// Exercicio 02
+// let resultado = document.querySelector('#resultado')
+// let somaNumeros = 0
+// notas = []
+
+// function pegaNotas() {
+//   for (let i = 0; i < 4; i++) {
+//     if (notas = Number) {
+//       notas.push(prompt('Qual são as suas notas ?'))
+//       somaNumeros = somaNumeros + notas[i]
+//       console.log(notas)
+//       console.log(somaNumeros)
+//     }
+//     else {
+//       alert('Número Inválido')
+//     }
+//   }
+// }
+
+// pegaNotas()
+
+// function calculaMedia() {
+//   let media = somaNumeros / notas.length
+//   console.log(media)
+// }
+
+// calculaMedia()
+
+// resultado.innerHTML = `<h1>A media das notas informadas é igual a: ${media}</h1>`
+
+// Exercicio 03
+
+// function mostraFrase(text) {
+//   console.log(`Olá ${text} !`)
+// }
+
+// mostraFrase("Gabriel")
+
+// Exercicio 04
+
+// function calculaNumeros(n1, n2) {
+//   console.log(n1 + n2)
+//   console.log(n1 - n2)
+//   console.log(n1 / n2)
+//   console.log(n1 * n2)
+// }
+
+// calculaNumeros(5, 7)
+
+// Exercicio 05
+
+// function confereNumero(num) {
+//   console.log(num % 3 == 0 ? true : false);
+// }
+
+// confereNumero(5)
+
+// Exercicio 06
+
+// function mostraMes(mesAno) {
+//   const mesesAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+  
+//   switch (mesAno) {
+//     case 1:
+//       console.log(mesesAno[0])
+//       break;
+//     case 2:
+//         console.log(mesesAno[1])
+//       break;
+//     case 3:
+//       console.log(mesesAno[2])
+//       break;
+//     case 4:
+//       console.log(mesesAno[3])
+//       break;
+//     case 5:
+//       console.log(mesesAno[4])
+//       break;
+//     case 6:
+//       console.log(mesesAno[5])
+//       break;
+//     case 7:
+//       console.log(mesesAno[6])
+//       break;
+//     case 8:
+//       console.log(mesesAno[7])
+//       break;
+//     case 9:
+//       console.log(mesesAno[8])
+//       break;
+//     case 10:
+//       console.log(mesesAno[9])
+//       break;
+//     case 11:
+//       console.log(mesesAno[10])
+//       break;
+//     case 12:
+//       console.log(mesesAno[11])
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+// mostraMes(1)
+
+// Exercicio 07
+
+// function verificaNumeros(n1, n2) {
+//   console.log(n1 >= n2 ? true : false)
+// }
+
+// verificaNumeros(2, 3)
