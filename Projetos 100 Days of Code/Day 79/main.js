e13079df4786f21c68562d8b3a613e60b57332e3
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2022-12-15 20:24:37 -0300

    Add files via upload

---
+++ Projetos 100 Days of Code/Day 79/main.js
@@ -0,0 +1,28 @@
+// Exercicio 1
+// let resultado = document.querySelector('#resultado')
+
+// function mostraTela() {
+//   resultado.innerHTML = '<h1>Olá Mundo !!!</h1>'
+// }
+
+// mostraTela()
+
+// Exercicio 2
+// let resultado = document.querySelector('#resultado')
+
+// let resposta = document.querySelector('#resposta')
+
+// let envia = document.querySelector('#envia')
+
+// function calculaCuboNumero() {
+//   envia.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     let respostaUsuario = parseFloat(resposta.value)
+//     console.log(respostaUsuario)
+
+//     console.log(respostaUsuario + respostaUsuario)
+//   })
+
+// }
+
+// calculaCuboNumero()
