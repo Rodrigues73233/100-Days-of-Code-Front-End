543bb0fc75e5d55a6224beb2ab880b35038dd0e1
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2022-12-18 23:39:36 -0300

    Add files via upload

---
+++ Projetos 100 Days of Code/Day 82/main.js
@@ -0,0 +1,42 @@
+// 01
+// let a = 'Vermelho'
+// let b = 'Azul'
+
+// let aVerdadeiro = b
+// let bVerdadeiro = a
+
+// console.log(aVerdadeiro, bVerdadeiro)
+
+// 02
+// function retornaMaior(a, b) {
+//   return a > b ? a : b
+// }
+
+// console.log(retornaMaior(2, 3))
+
+// 03
+// Divisível por 3 => Fizz
+// Divisível por 5 => Buzz
+// Divisível por 3 e 5 => FizzBuzz
+// Não divisível por 3 ou 5 => Entrada
+// Não é número => "Não é um número"
+
+// function FizzBuzz(entrada) {
+//   if (isNaN(entrada)) {
+//     return "Não é um número"
+//   }
+//   else if (entrada % 3 == 0 && entrada % 5 == 0) {
+//     return "FizzBuzz"
+//   }
+//   else if (entrada % 3 == 0) {
+//     return "Fizz"
+//   }
+//   else if (entrada % 5 == 0) {
+//     return "Buzz"
+//   }
+//   else {
+//     return entrada
+//   }
+// }
+
+// console.log(FizzBuzz(92))
