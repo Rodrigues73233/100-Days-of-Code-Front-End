5cceaeb616bb91fdad90c7219e1601cb4f7d3ad9
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2023-01-01 17:42:55 -0300

    Add files via upload

---
+++ Projetos 100 Days of Code/Day 96/Exercicio-1/main.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+
+  let getSeconds = document.querySelector('#seconds')
+  let getMiliSeconds = document.querySelector('#miliseconds')
+  
+  let startBtn = document.querySelector('#button-start')
+  let stopBtn = document.querySelector('#button-stop')
+  let resetBtn = document.querySelector('#button-reset')
+
+  let seconds = 00;
+  let miliseconds = 00;
+
+  let interval;
+
+  startBtn.addEventListener('click', () => {
+    interval = setInterval(startTimer, 10)
+  })
+
+  function startTimer() {
+    miliseconds++
+    
+    if(miliseconds <= 9) {
+      getMiliSeconds.innerHTML = "0" + miliseconds
+    }
+    if(miliseconds > 9) {
+      getMiliSeconds.innerHTML = miliseconds
+    }
+    if(miliseconds > 99) {
+      seconds++
+      getMiliSeconds.innerHTML = "00"
+      miliseconds = 00
+    }
+    if(seconds <= 9) {
+      getSeconds.innerHTML = "0" + seconds
+    }
+    if(seconds > 9) {
+      getSeconds.innerHTML = seconds
+    }
+  }
+
+  stopBtn.addEventListener('click', () => {
+    clearInterval(interval)
+  })
+
+  resetBtn.addEventListener('click', () => {
+    clearInterval(interval)
+    seconds = 00
+    miliseconds = 00
+    getSeconds.innerHTML = "00"
+    getMiliSeconds.innerHTML = "00"
+  })
+}
